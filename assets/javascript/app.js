adb97dd8b21bb021ee369bc37774555fe7260f72
Yirui Chen <yiruic@users.noreply.github.com>
2017-12-14 15:44:31 -0800

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -68,10 +68,8 @@
           // Looping over every result item
           for (var i = 0; i < results.length; i++) {
 
-            // Only taking action if the photo has an appropriate rating
-            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
               // Creating a div with the class "item"
-              var gifDiv = $("<div class='item'>");
+              var gifDiv = $("<div>");
 
               // Storing the result item's rating
               var rating = results[i].rating;
@@ -93,6 +91,22 @@
               // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
               $("#gifs-appear-here").prepend(gifDiv);
             }
-          }
+          })
         });
-      });
+
+
+//pausing GIFs
+$("<img>").on("click", function() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
