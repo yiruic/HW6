5721e3c4353ab319edaa6be8b5c1a3a24a05ef3c
Yirui Chen <yiruic@users.noreply.github.com>
2017-12-15 16:16:31 -0800

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 // Initial array of artists
-      var artists = ["Ed Sheeran", "Coldplay", "Sam Smith", "Sia", "Amy Winehouse"];
+      var artists = ["Ed Sheeran", "Arctic Monkeys", "Sam Smith", "Sia", "Amy Winehouse"];
 
-// Function for displaying artist data
+
+  // Function for displaying artist data
       function renderButtons() {
 
         $("#artists-view").empty();
@@ -9,52 +10,54 @@
         // Looping through the array of movies
         for (var i = 0; i < artists.length; i++) {
 
+          // console.log(i);
+
           // Then dynamicaly generating buttons for each movie in the array.
           var a = $("<button>");
 
-          // Adding a class
-          a.addClass('artist');
-
           // Adding a data-attribute with a value of the movie at index i
-          a.attr("name", artists[i]);
+          a.attr("data-name", artists[i]);
 
           // Providing the button's text with a value of the movie at index i
           a.text(artists[i]);
 
           // Adding the button to the HTML
           $('#artists-view').append(a); 
-          }
-        }
-      // This function handles events where one button is clicked
-      $("#add-artist").on("click", function(event) {
-        // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
-        event.preventDefault();
+          };
+        };
 
-        // This line will grab the text from the input box
-        var artist = $("#artist-input").val().trim();
-        // The artist from the textbox is then added to our array
-        artists.push(artist);
+        // This function handles events where one button is clicked
+        $("#add-artist").on("click", function(event) {
+          // event.preventDefault() prevents the form from trying to submit itself.
+          // We're using a form so that the user can hit enter instead of clicking the button if they want
+          event.preventDefault();
 
-        // calling renderButtons which handles the processing of our movie array
-        renderButtons();
-      });
+          // This line will grab the text from the input box
+          var artistInput = $("#artist-input").val().trim();
+          // The artist from the textbox is then added to our array
+
+          artists.push(artistInput);
+          console.log(artistInput);
+          console.log(artists);
+
+          // calling renderButtons which handles the processing of our movie array
+          renderButtons();
+        });
+
+
 
       // Calling the renderButtons function at least once to display the initial list of movies
       renderButtons();
     
 
-
-
     // Event listener for all button elements
     $("button").on("click", function() {
       // In this case, the "this" keyword refers to the button that was clicked
-      var artist = $(this).attr("data-artist");
-    
+      var artistButton = $(this).attr("data-name");
 
       // Storing our giphy API URL for a searched artist image
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + artist + "&api_key=jszR2M7ux83PUtTDzZzD7ZjZOM77lLqk";
-
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + artistButton + "&api_key=jszR2M7ux83PUtTDzZzD7ZjZOM77lLqk&limit=5";
+      
       $.ajax({
         url: queryURL,
         method: "GET"
@@ -65,11 +68,15 @@
           // Storing an array of results in the results variable
           var results = response.data;
 
+          console.log(queryURL);
+
+          console.log(response);
+
           // Looping over every result item
           for (var i = 0; i < results.length; i++) {
 
               // Creating a div with the class "item"
-              var gifDiv = $("<div>");
+              var gifDiv = $("<div>", 'item');
 
               // Storing the result item's rating
               var rating = results[i].rating;
@@ -91,14 +98,14 @@
               // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
               $("#gifs-appear-here").prepend(gifDiv);
             }
-          })
+          });
         });
 
 
 //pausing GIFs
-$("<img>").on("click", function() {
+$(".gifDiv").on("click", function() {
       // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-      var state = $(this).attr("data-state");
+      var state = $(this).attr("difDiv");
       // If the clicked image's state is still, update its src attribute to what its data-animate value is.
       // Then, set the image's data-state to animate
       // Else set src to the data-still value
